7d9b1bd804112a3eded40703ed31a5d8fdb810b7
Dr D <85292902+dilshadalisirohi@users.noreply.github.com>
2023-02-01 22:14:52 +0500

    Updated

--- app.js
+++ app.js
@@ -3,22 +3,10 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", navToggle);
 function navToggle() {
   const nav = document.querySelector(".nav-list");
+  const ham = document.querySelector(".hamburger-icon");
+  ham.classList.toggle("open");
   nav.classList.toggle("responsive");
 }
-
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       document.querySelector(".navbar").classList.add("navbar-move");
-//     } else {
-//       document.querySelector(".navbar").classList.remove("navbar-move");
-//     }
-//   });
-// });
-
-// const trigger = document.querySelectorAll(".nav-trigger");
-
-// trigger.forEach((el) => observer.observe(el));
 
 window.addEventListener("scroll", (e) => {
   if (window.scrollY > 30) {
